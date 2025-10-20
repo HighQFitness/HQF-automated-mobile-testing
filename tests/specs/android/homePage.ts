db26acd7615ac4d206d9c8c49f50ad8793a21656
Jimena Nemiña <jimenanemina@Jimenas-MacBook-Pro.local>
2025-10-20 18:48:12 -0300

    fist working android test

--- tests/specs/android/homePage.ts
+++ tests/specs/android/homePage.ts
@@ -1,18 +1,17 @@
 import { expect } from "chai";
+import { ChainablePromiseElement } from "webdriverio";
 
 describe("Android App Launch", () => {
   it("should open the app and find Home", async () => {
     const activity = await driver.getCurrentActivity();
     console.log("📱 Current activity:", activity);
 
-    let element;
-    element = await $('android=new UiSelector().text("Home")');
+    let element: ChainablePromiseElement;
+    element = $('android=new UiSelector().text("Home")');
       await element.waitForExist({ timeout: 5000 });
       console.log("'Home' element found!");
 
     await element.click();
-    console.log("👉 Clicked element successfully.");
-
     expect(await driver.getCurrentActivity()).to.not.be.empty;
   });
 });
