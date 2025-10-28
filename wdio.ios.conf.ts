9ea7eba2ed9ee9d15c3f1799a2d5c72232556498
Jimena Nemiña <jimenanemina@Jimenas-MacBook-Pro.local>
2025-10-27 21:19:19 -0300

    made changes to wdio files

--- wdio.ios.conf.ts
+++ wdio.ios.conf.ts
@@ -137,7 +137,16 @@ export const config: WebdriverIO.Config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: [
+  'spec',
+  ['junit', {
+    outputDir: `./reports/${process.env.PLATFORM || 'iOS'}`,
+    outputFileFormat: function (opts) {
+      return `results-${opts.cid}.xml`;
+    },
+  }],
+],
+
 
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
